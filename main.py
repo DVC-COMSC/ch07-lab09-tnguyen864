ab94f36e2bb82e671c07f09ed923496ad0c92537
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 04:51:05 -0700

    Chap7 Lab9

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-# ******************************
-# Make your Code
-# ******************************
+# List for 5 names
+names = []
+
+# Get 5 names
+for i in range(5):
+    name = input("Enter a name: ")
+    names.append(name)
+
+# Sort list
+names.sort()
+
+# Get shortest and longest string
+shortest = min(names, key = len)
+longest = max(names, key = len)
+
+# Print shortest and longest string
+print("Shortest:", shortest)
+print("Longest:", longest)
